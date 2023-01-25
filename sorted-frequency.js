83abe4408f5fa32c3ebddc9c4ae9da76acc6f6b0
Rachael Shipes <Rachael@Rachaels-MacBook-Pro-2.local>
2023-01-25 16:31:11 -0500

    given a sorted array and a number, write a function called sortedFrequency that counts the occurrences of the number in the array

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -2,9 +2,50 @@
 
 // Time Complexity: O(log N)
 
+function findFirst(arr, num, left = 0, right = arr.length - 1) {
+    if (right >= left) {
 
-function sortedFrequency() {
+      let middle = Math.floor((left+ right) / 2)
 
-}
+      if ((middle === 0 || num > arr[middle - 1]) && arr[middle] === num) {
+        return middle;
+      } else if (num > arr[middle]) {
+        return findFirst(arr, num, middle + 1, right)
+      } else {
+        return findFirst(arr, num, left, middle - 1)
+      }
+    }
+
+    return -1
+  }
+  
+  function findLast(arr, num, left = 0, right = arr.length - 1) {
+    if (right >= left) {
+
+      let middle = Math.floor((left + right) / 2)
+
+      if ((middle === arr.length - 1 || num < arr[middle + 1]) && arr[middle] === num) {
+        return middle;
+      } else if (num < arr[middle]) {
+        return findLast(arr, num, left, middle- 1)
+      } else {
+        return findLast(arr, num, middle + 1, right)
+      }
+    }
+
+    return -1
+  }
+
+  function sortedFrequency(arr, num) {
+    let first = findFirst(arr, num);
+
+    if (first == -1) {
+        return first;
+    }
+
+    let last = findLast(arr, num);
+
+    return last - first + 1;
+  }
 
 module.exports = sortedFrequency
